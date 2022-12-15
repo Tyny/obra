69cf6f2b43d0b74db96c351929f8497a8a90dff3
Martin Silva <martin.silva@cedalio.com>
2022-12-15 10:03:24 -0300

    borrowing

--- workspaces/borrowing/src/main.rs
+++ workspaces/borrowing/src/main.rs
@@ -1,3 +1,5 @@
+// Saludemos a compas de factic con un vector de nombres y tratemos de agregar a alguien
+
 fn main() {
-    println!("Baila Gaia! Hasta abajooo");
+    println!("Baila Gaiaaa hasta abajoo!");
 }
